d89e9cfe92e6a274cd9fd03041c413f760ac9abc
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-14 11:03:41 -0500

    Finish Step 3

--- SharedCode/AbstractFileSystem.h
+++ SharedCode/AbstractFileSystem.h
@@ -1,2 +1,17 @@
 #pragma once
 // declaration of the interface all file systems provide goes here
+#include <string>
+#include "AbstractFile.h"
+
+
+class AbstractFileSystem {
+public:
+
+	virtual int addFile(std::string fileName, AbstractFile* filePtr) = 0;
+	virtual int createFile(std::string fileName) = 0;
+	virtual int deleteFile(std::string fileName) = 0;
+	virtual AbstractFile* openFile(std::string fileName) = 0;
+	virtual int closeFile(AbstractFile* filePtr) = 0;
+
+
+};
--- Studio17/Studio17/ReadMe.txt
+++ Studio17/Studio17/ReadMe.txt
@@ -5,4 +5,5 @@ Team member: Jinfeng Chen, Daniel Ryu
 2. Multiple tests are ran to make sure the ImageFile type does not support append(), and write() function can give unique return 
 	codes for bad inputs.
 
-3.
+3. The benefits of using an interface instead of concrete classes allow easy future extension to the program, because changing the acutal 
+	implementation of the underlying code would not affect how client code interact with the interface.
