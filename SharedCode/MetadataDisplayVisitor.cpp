83ca34144087f1e9d0e15949a742a7653f70eda5
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-21 10:39:50 -0500

    Start Studio 19 Step 5

--- SharedCode/MetadataDisplayVisitor.cpp
+++ SharedCode/MetadataDisplayVisitor.cpp
@@ -1,1 +1,13 @@
 // definitions of metadata visitor here
+
+#include "MetadataDisplayVisitor.h"
+
+using namespace std;
+
+void MetadataDisplayVisitor::visit_TextFile(TextFile* filePtr) {
+
+}
+
+void MetadataDisplayVisitor::visit_ImageFile(ImageFile* filePtr) {
+
+}
