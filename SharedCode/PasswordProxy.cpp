a3e2730528071f57c0c98a154a199e4e2fab4ec8
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-23 17:04:49 -0500

    Start PasswordProxy

---
+++ SharedCode/PasswordProxy.cpp
@@ -0,0 +1,5 @@
+#include "PasswordProxy.h"
+#include <iostream>
+
+using namespace std;
+
--- SharedCode/PasswordProxy.h
+++ SharedCode/PasswordProxy.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <vector>
+#include "AbstractFile.h"
+
+
+class PasswordProxy : public AbstractFile {
+
+public:
+	std::vector<char> read() override;
+		
+	int write(std::vector<char> v) override;
+	int append(std::vector<char> v) override;
+	unsigned int getSize() override;
+	std::string getName() override;
+
+};
