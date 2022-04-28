070edf4e2c087a973c84b372cc148d50b3cd0529
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-28 11:08:37 -0500

    Add setw() to MetaDisplayVisitor()

--- SharedCode/MetadataDisplayVisitor.cpp
+++ SharedCode/MetadataDisplayVisitor.cpp
@@ -1,19 +1,20 @@
 // definitions of metadata visitor here
 
 #include "MetadataDisplayVisitor.h"
+#include <iomanip>
 
 using namespace std;
 
 void MetadataDisplayVisitor::visit_TextFile(TextFile* filePtr) {
-	cout << filePtr->getName();
-	cout << "text";
+	cout << setw(20) << filePtr->getName();
+	cout << setw(10) << "text";
 	cout << filePtr->getSize() << endl;
 	
 }
 
 void MetadataDisplayVisitor::visit_ImageFile(ImageFile* filePtr) {
-	cout << filePtr->getName() ;
-	cout << "image" ;
+	cout << setw(20) << filePtr->getName() ;
+	cout << setw(10) <<  "image" ;
 	cout << filePtr->getSize() << endl;
 	
 }
