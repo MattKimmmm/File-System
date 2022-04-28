23998d6efabf4eee080b65885e0c6f393ead6301
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-28 10:39:09 -0500

    Build LSCommand class

--- SharedCode/LSCommand.h
+++ SharedCode/LSCommand.h
@@ -1,1 +1,13 @@
 #pragma once
+
+
+#include <iostream>
+
+#include "AbstractCommand.h"
+
+class LSCommand: public AbstractCommand {
+
+public:
+	int execute(std::string intput) override;
+	void displayInfo() override;
+};
