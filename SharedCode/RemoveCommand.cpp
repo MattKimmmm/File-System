6799e196563cbeb5d6c041baa840a9171db7f24e
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-30 11:53:04 -0500

    Start removeCommand execute()

---
+++ SharedCode/RemoveCommand.cpp
@@ -0,0 +1,23 @@
+#include "RemoveCommand.h"
+#include "SimpleFileSystem.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+RemoveCommand::RemoveCommand(AbstractFileSystem* fileSys) : fileSysPtr(fileSys) {
+
+}
+
+void RemoveCommand::displayInfo() {
+	cout << "Remove removes a file from the system, remove can be invoked with the command: rm <filename>" << endl;
+}
+
+int RemoveCommand::execute(string input) {
+	
+	
+		fileSysPtr->deleteFile
+	
+
+}
--- SharedCode/RemoveCommand.h
+++ SharedCode/RemoveCommand.h
@@ -1,3 +1,18 @@
 #pragma once
 
+#include "AbstractCommand.h"
+#include "AbstractFileSystem.h"
 
+class RemoveCommand :public AbstractCommand {
+private:
+	
+	AbstractFileSystem* fileSysPtr;
+
+public:
+	virtual int execute(std::string intput);
+	virtual void displayInfo();
+
+	RemoveCommand(AbstractFileSystem * fileSys);
+
+
+};
