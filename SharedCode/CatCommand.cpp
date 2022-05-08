69a10ba4b61b776f7290efedf750745657673eaf
c.jinfeng <c.jinfeng@wustl.edu>
2022-05-08 10:53:58 -0500

    Add printout to guide users at cat command

--- SharedCode/CatCommand.cpp
+++ SharedCode/CatCommand.cpp
@@ -28,6 +28,11 @@ int CatCommand::execute(string input) {
 		vector<char> overwriteContent = {}; //contents to overwrite to file.
 		
 		while (true) {
+
+			cout << endl;
+			cout << "Enter data you would like to write to the file. Enter :wq to sace the file and exit,"
+				<< " enter :q to exist without saving and without saving and without -a, we don't append but overwrite the old contents" << endl;
+
 			getline(cin,userInput);
 			if (!(userInput == ":wq") && !(userInput == ":q")) {
 				string contentInput;
