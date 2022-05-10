1f82a5193a0950325f333ad6b2a156a520d7b930
c.jinfeng <c.jinfeng@wustl.edu>
2022-05-10 17:07:12 -0500

    Finish displayInfo() of ChmodCommand

--- SharedCode/ChmodCommand.cpp
+++ SharedCode/ChmodCommand.cpp
@@ -4,6 +4,7 @@
 #include "AbstractFileSystem.h"
 #include "ChmodProxy.h"
 #include "ChmodCommand.h"
+#include <iostream>
 
 using namespace std;
 
@@ -14,7 +15,8 @@ int ChmodCommand::execute(std::string input) {
 
 
 void ChmodCommand::displayInfo() {
-
+	cout << "chmod changes the file to and from read-only, chmod <file_name> - makes the file read-only, " <<
+		" and chmod <file_name> + makes the file writable again" << endl;
 };
 
 ChmodCommand::ChmodCommand(AbstractFileSystem* sys):fileSys(sys) {
